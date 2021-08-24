70df38f6dba0ab306f55f0680a72324f62ffd265
Simon <330771794@qq.com>
2021-08-24 20:03:01 +0800

    新增一些void的注释，优化小部件的代码

--- app/src/main/java/com/ahu/ahutong/utils/DateUtils.java
+++ app/src/main/java/com/ahu/ahutong/utils/DateUtils.java
@@ -4,6 +4,10 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtils {
+    /**
+     * 获取当前是周几
+     * @return 返回 周几
+     */
     public static String getWeek() {
         String[] weeks={"周日","周一","周二","周三","周四","周五","周六"};
         Calendar c = Calendar.getInstance();
