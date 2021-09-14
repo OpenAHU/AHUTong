764ff60d56386febfb38a7ca4a57984b47c6ce11
Simon <330771794@qq.com>
2021-09-14 12:19:13 +0800

    更改版本1.0.0-beta3的更新日志

--- app/src/main/java/com/ahu/ahutong/Constants.java
+++ app/src/main/java/com/ahu/ahutong/Constants.java
@@ -6,10 +6,8 @@ package com.ahu.ahutong;
  * @Email: 468766131@qq.com
  */
 public class Constants {
-    public static final String UPDATE_LOG = "版本：1.0.0-beta1\n" +
-            "- 测试版本初步实现基本功能\n" +
-            "- 添加校园卡余额查询\n" +
-            "- 添加桌面小部件\n" +
-            "- 移除小程序中无用功能\n" +
-            "- 图书馆、校园一卡通布局中，预计正式版加入";
+    public static final String UPDATE_LOG = "版本：1.0.0-beta3\n" +
+            "- 部分功能增加热更新。\n"+
+            "- 完善检查更新。\n"+
+            "- 修复小部件BUG。\n";
 }
