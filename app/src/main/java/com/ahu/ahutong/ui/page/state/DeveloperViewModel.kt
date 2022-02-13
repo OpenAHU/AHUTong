f89e739e39184b3a67813702baa0d7b8bdc8ca1f
Simon <330771794@qq.com>
2022-02-13 11:17:47 +0800

    完善了开发者页面的小细节

--- app/src/main/java/com/ahu/ahutong/ui/page/state/DeveloperViewModel.kt
+++ app/src/main/java/com/ahu/ahutong/ui/page/state/DeveloperViewModel.kt
@@ -14,7 +14,7 @@ class DeveloperViewModel: ViewModel() {
         listOf(
 //            Developer("吴振龙", "爬虫&后端开发者", "1298749337"),
             Developer("高玉灿", "Android 客户端 & 爬虫 开发者", "468766131"),
-            Developer("谭哲昊", "Android 客户端开发者","330771794")
+            Developer("谭哲昊", "Android 客户端 & 摸鱼 开发者","330771794")
         )
     }
 }
