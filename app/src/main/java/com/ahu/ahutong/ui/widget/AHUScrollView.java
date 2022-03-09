d6659c6d54472a64065d7a5b43a04963dbc5ced8
Sink <468766131@qq.com>
2022-03-09 15:19:09 +0800

    AHUScrollView（适配课表在小屏幕下的点击问题） 提交

---
+++ app/src/main/java/com/ahu/ahutong/ui/widget/AHUScrollView.java
@@ -0,0 +1,41 @@
+package com.ahu.ahutong.ui.widget;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.widget.NestedScrollView;
+
+
+public class AHUScrollView extends NestedScrollView {
+    public AHUScrollView(@NonNull Context context) {
+        super(context);
+    }
+
+    public AHUScrollView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public AHUScrollView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    private int lastScrollY = 0;
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        scrollTo(0, lastScrollY);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        boolean b = super.onTouchEvent(ev);
+        lastScrollY = getScrollY();
+        return b;
+    }
+}
