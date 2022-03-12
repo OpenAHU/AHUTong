97bd22f0f4b9c71366ec8cc2df7ffb8d93ed1cf5
Simon <330771794@qq.com>
2022-03-12 09:46:14 +0800

    加入后期名单

--- app/src/main/java/com/ahu/ahutong/ui/page/state/DeveloperViewModel.kt
+++ app/src/main/java/com/ahu/ahutong/ui/page/state/DeveloperViewModel.kt
@@ -15,6 +15,9 @@ class DeveloperViewModel : ViewModel() {
             Developer("高玉灿", "Android 客户端 & 划水 开发者", "468766131"),
             Developer("谭哲昊 (在线求偶)", "Android 客户端 & 摸鱼 开发者", "330771794"),
             Developer("吴振龙", "爬虫 & 后端 开发者", "1298749337"),
+            Developer("徐海", "后端 & 卷王 开发者", "1479356730"),
+            Developer("王岳赣", "后端 & 芭比Q 开发者", "1491605298"),
+            Developer("王壮壮", "后端 & 摸鱼 开发者", "2539907983"),
         )
     }
 }
