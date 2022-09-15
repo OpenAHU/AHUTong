b06157f41c2fb84c13e54387bbe20418f37ab353
17390970692 <1qwertyuiop>
2022-09-15 15:27:43 +0800

    删除了空教室查询，删除了垃圾分类查询，删除了more页面

--- app/src/main/java/com/ahu/ahutong/ui/page/state/DeveloperViewModel.kt
+++ app/src/main/java/com/ahu/ahutong/ui/page/state/DeveloperViewModel.kt
@@ -13,9 +13,10 @@ class DeveloperViewModel : ViewModel() {
     val developers by lazy {
         listOf(
             Developer("高玉灿", "Android 客户端 & 划水 开发者", "468766131"),
-            Developer("谭哲昊 (在线求偶)", "Android 客户端 & 摸鱼 开发者", "330771794"),
+            Developer("谭哲昊", "Android 客户端 & 摸鱼 开发者", "330771794"),
+            Developer("王学雷", "Android 客户端 & 美化 开发者", "257314409"),
             Developer("吴振龙", "爬虫 & 后端 开发者", "1298749337"),
-            Developer("徐海", "后端 & 卷王 开发者", "1479356730"),
+            Developer("徐 海", "后端 & 卷王 开发者", "1479356730"),
             Developer("王岳赣", "后端 & 芭比Q 开发者", "1491605298"),
             Developer("王壮壮", "后端 & 摸鱼 开发者", "2539907983"),
         )
