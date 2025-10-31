e76813da6314d69a67dcbba4bc95b0bdcb3b431c
akua <simon@akua.fan>
2025-11-01 00:34:29 +0800

    [Optimize] remove old struct

--- app/src/main/java/com/ahu/ahutong/AHUApplication.java
+++ app/src/main/java/com/ahu/ahutong/AHUApplication.java
@@ -1,6 +1,8 @@
 package com.ahu.ahutong;
 
-import arch.sink.BaseApplication;
+import android.app.Application;
+import android.content.Context;
+
 import dagger.hilt.android.HiltAndroidApp;
 
 /**
@@ -10,14 +12,23 @@ import dagger.hilt.android.HiltAndroidApp;
  */
 
 @HiltAndroidApp
-public class AHUApplication extends BaseApplication {
+public class AHUApplication extends Application {
 
     public volatile static Boolean sessionExpired = true;
     public volatile static Object reLoginMutex = new Object();
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
     }
 
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
 }
