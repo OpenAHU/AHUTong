75534c07b823ab4e2a1fd1c9d407e7612588777e
akua <simon@akua.fan>
2025-10-31 23:32:44 +0800

    [Optimize] remove webView reptile

--- app/src/main/java/com/ahu/ahutong/ext/StringExt.kt
+++ app/src/main/java/com/ahu/ahutong/ext/StringExt.kt
@@ -8,19 +8,6 @@ import java.lang.reflect.Type
  * @Date: 2021/7/31-下午8:58
  * @Email: 468766131@qq.com
  */
-
-fun String.isCampus(): Boolean {
-    return this == "1" || this == "2"
-}
-
-fun String.isWeekday(): Boolean {
-    return this in listOf("1", "2", "3", "4", "5", "6", "7")
-}
-
-fun String.isEmptyRoomTime(): Boolean {
-    return this in listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "10")
-}
-
 fun String.isTerm(): Boolean {
     return this == "1" || this == "2"
 }
